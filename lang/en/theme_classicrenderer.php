3f0f3ad732714e355c0d14b064f2ee07b3d1607f
TIm St.Clair <tim.stclair@gmail.com>
2022-11-28 06:29:20 +1100

    enrol wording changes

--- lang/en/theme_classicrenderer.php
+++ lang/en/theme_classicrenderer.php
@@ -4,3 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Classic Renderer';
 $string['returntext'] = 'Return to course home';
+
+$string['notenrollable'] = 'Sorry, you have tried to access an area that you are not permitted to view.';
+$string['enrolmentoptions'] = 'Permission error';
